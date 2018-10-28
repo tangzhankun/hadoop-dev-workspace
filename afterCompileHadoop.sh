a2cd9f7e8a44bbee8e9928e8fadbc16842ecee7f
tangzhankun <tangzhankun@gmail.com>
2018-10-28 21:20:56 +0800

    use daemon to start and set log level

--- afterCompileHadoop.sh
+++ afterCompileHadoop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-stop-yarn.sh
+> ../logs/yarn/hadoop-yarn-nodemanager-master0-VirtualBox.log
+source env.sh
+yarn --daemon stop resourcemanager
+yarn --loglevel DEBUG --daemon stop nodemanager
+#stop-yarn.sh
 sleep 1
 sudo mkdir /opt/code/hadoop/hadoop-dist/target/hadoop-3.3.0-SNAPSHOT/logs
 sudo chown -R root:hadoop /opt/code/hadoop/hadoop-dist
@@ -12,5 +16,7 @@ sudo chmod 6050 ../home/bin/container-executor
 sudo chmod 0400 ../home/etc/hadoop/container-executor.cfg
 
 #put some vendor plugin
-sudo cp ../ve-1.0-SNAPSHOT.jar ../home/share/hadoop/yarn/
-start-yarn.sh
+./copy_device_plugins.sh
+#start-yarn.sh
+yarn --daemon start resourcemanager
+yarn --loglevel DEBUG --daemon start nodemanager
