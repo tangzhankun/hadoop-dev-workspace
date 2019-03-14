7f101cf9564db8246e77af7b2482c9e5c9f0cc19
tangzhankun <tangzhankun@gmail.com>
2019-03-14 16:36:48 +0800

    submarine demo Spain

---
+++ demo_tensorboard.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#VERSION=3.2.0
+VERSION=3.3.0-SNAPSHOT
+hdfs dfs -rm -r .yarn/services/tf-board-001
+hdfs dfs -rm -R -f submarine/jobs/tf-board-001
+yarn jar $HADOOP_COMMON_HOME/share/hadoop/yarn/hadoop-yarn-submarine-${VERSION}.jar job run \
+  -verbose \
+  -wait_job_finish \
+  --env DOCKER_JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 \
+  --env DOCKER_HADOOP_HDFS_HOME=/hadoop-3.1.2 \
+  --env YARN_CONTAINER_RUNTIME_DOCKER_CONTAINER_NETWORK=bridge \
+  --input_path hdfs://default/user/yarn/cifar-10-data  \
+  --checkpoint_path hdfs://default/user/yarn/cifar-10-jobdir \
+  --name tf-board-001 \
+  --docker_image tangzhankun/tensorflow:demo \
+  --worker_resources memory=1G,vcores=1  \
+  --tensorboard \
+  --num_workers 0 \
+  --tensorboard_docker_image tangzhankun/tensorboard:demo
+
+# for test purpose
+#  --worker_launch_cmd "cat /proc/1/cgroup"
+# --checkpoint_path hdfs://default/user/yarn/cifar-10-jobdir \
+#  --checkpoint_path hdfs://default/user/yarn/submarine/jobs/tf-job-001/staging \
+#  --input_path hdfs://default/user/yarn/cifar-10-data  \
+#  -keep_staging_dir \
+#  --tensorboard \
+#  --quicklink notebook_ui="https://master-0:7070" \
---
+++ env-3.3.0-SNAPSHOT.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export HADOOP_BASE_DIR=${HOME}/apache-hadoop-install-dir
+#export HADOOP_LOG_DIR=${HOME}/apache-hadoop-install-dir/logs
+export HADOOP_LOG_DIR=/opt/yarn-logs
+export HADOOP_PID_DIR=${HADOOP_BASE_DIR}
+export HADOOP_CONF_DIR=${HOME}/apache-hadoop-install-dir/hadoop-dev-workspace/conf_3.3.0-SNAPSHOT
+export HADOOP_COMMON_HOME=${HOME}/apache-hadoop-install-dir/home_3.3.0-SNAPSHOT
+export HADOOP_HDFS_HOME=${HADOOP_COMMON_HOME}
+export HADOOP_MAPRED_HOME=${HADOOP_COMMON_HOME}
+# Yarn env override:
+export HADOOP_YARN_HOME=${HADOOP_COMMON_HOME}
+export YARN_LOG_DIR=${HADOOP_LOG_DIR}/yarn
+#classpath override to search hadoop loc
+export CLASSPATH=/usr/share/java/:${HADOOP_COMMON_HOME}/share
+#Finally update your PATH
+export PATH=${HADOOP_COMMON_HOME}/bin:${HADOOP_COMMON_HOME}/sbin:${HADOOP_COMMON_HOME}/libexec:${PATH}
